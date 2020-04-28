da75fa6a9c6d364f4d3f16a34607446d9fd39296
Ricardo Russ <ricardoruss@live.com>
2020-04-28 16:49:45 -0400

    changing port to heroku determined

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose.connect(MONGODB_URI);
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`App running on port ${PORT}!`);
 });
 
